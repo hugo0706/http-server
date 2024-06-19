98a83f17c94da69e381fdd2f5eeaceabdf475fc5
hugo0706 <hugo.garciac@edu.uah.es>
2024-06-19 19:24:10 +0200

    Task: React to different targets in request
    Built an HttpRequest object to parse the request

---
+++ app/http_request.rb
@@ -0,0 +1,28 @@
+class HttpRequest
+
+  attr_accessor :headers, :body, :http_method , :target , :http_version
+
+  def initialize(request_line:, headers:, body:)
+    parse_request_line(request_line)
+    parse_headers(headers)
+    parse_body(body)
+  end
+
+  private
+
+  def parse_request_line(request_line)
+    @http_method, @target, @http_version = request_line.split(' ')
+  end
+
+  def parse_headers(headers)
+    @headers = {}
+    headers.each do |header|
+      key, value = header.split(': ')
+      @headers[key] = value
+    end
+  end
+
+  def parse_body(body)
+    @body = body
+  end
+end
--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,30 @@
 require "socket"
+require_relative "http_request"
 
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
 
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+request_line = client_socket.gets
+headers = []
+body = nil
+
+while (line = client_socket.gets) && (line !="\r\n")
+  headers << line.chomp
+end
+
+content_length = headers.find {|header| header.start_with?('Content-Lenght:')}
+
+if content_length
+  body_lenght = content_length.split(': ').last.to_i
+  body = client_socket.read(lenght)
+end
+
+parsed_request = HttpRequest.new(request_line: request_line, headers: headers, body: body)
+
+if parsed_request.target == '/'
+  client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+else
+  client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+end
+
+server.close
